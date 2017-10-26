704d8450c295ed9987f67614e3fb64ecf29434c0
Brian Clinkenbeard <bclinkenbeard0@saddleback.edu>
2017-10-25 17:53:17 -0700

    more style

--- src/login.cpp
+++ src/login.cpp
@@ -19,22 +19,19 @@ bool login::isAccount(QString nameIn, QString passIn)
     /*
      * Input: STRING username guess, STRING password guess
      * Output: BOOL true if found in accounts vector, false if otherwise
-     *
      */
-    //this will be returned as the state of the inputted variables
-    bool isValid = false;
-
+    // this will be returned as the state of the inputted variables
     for (int index = 0; index < accounts.size(); index++) {
         if (nameIn == accounts[index].name && passIn == accounts[index].password)
-            isValid = true;
+            return true;
     }
 
-    return isValid;
+    // if no account found return false
+    return false;
 }
 
 bool login::isAdmin(QString nameIn, QString passIn)
 {
-    bool isAdmin = false; //returns true if the user is infact an admin
     int accountIndex;     //index of the account searched for
 
     for (int i = 0; i < accounts.size(); i++) {
@@ -44,21 +41,22 @@ bool login::isAdmin(QString nameIn, QString passIn)
 
     if (isAccount(nameIn,passIn)) {
        if (accounts.at(accountIndex).okAdmin == true)
-           isAdmin = true;
+           return true;
     }
 
-    return isAdmin;
+    // if no account return false
+    return false;
 }
 
 void login::addAccount(QString nameIn, QString passIn, bool adminIn)
 {
     user insert;
 
-    //assumes that the argument is valid variables
+    // assumes that the argument is valid variables
     insert.name = nameIn;
     insert.password = passIn;
     insert.okAdmin = adminIn;
-    //adds the inserted variables into the struct
+    // adds the inserted variables into the struct
     accounts.push_back(insert);
 
 }
